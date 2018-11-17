ba7a93ec277805021e4682d931f4f8a8b3838e60
Shyak Das <shayak@adview.com>
2018-11-17 17:23:19 +0530

    [Implementation]Share, like , backpress buttom implemented

--- app/src/main/java/com/jiva/com/jivaassigment/eventdetail/DetailActivity.java
+++ app/src/main/java/com/jiva/com/jivaassigment/eventdetail/DetailActivity.java
@@ -1,15 +1,42 @@
 package com.jiva.com.jivaassigment.eventdetail;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.jiva.com.jivaassigment.R;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView mBackPress, mShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        mBackPress = findViewById(R.id.back_arrow);
+        mShare = findViewById(R.id.share);
+        mShare.setOnClickListener(this);
+        mBackPress.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.back_arrow:
+                onBackPressed();
+                break;
+
+            case R.id.share:
+                String shareBody = "Here is the share content body";
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, getResources().getString(R.string.share)));
+                break;
+        }
     }
 }
